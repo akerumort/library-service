f323a21a204238540ad20dfd1d20859e61e6f374
Ivan Rodionov <144949516+akerumort@users.noreply.github.com>
2024-09-04 16:30:33 +0300

    added Flyway migration

---
+++ src/main/resources/db/migration/V1__Init_DB.sql
@@ -0,0 +1,21 @@
+CREATE TABLE author (
+    id SERIAL PRIMARY KEY,
+    first_name VARCHAR(255) NOT NULL,
+    last_name VARCHAR(255) NOT NULL,
+    county VARCHAR(255) NOT NULL
+);
+
+CREATE TABLE book (
+    id SERIAL PRIMARY KEY,
+    title VARCHAR(255) NOT NULL,
+    genre VARCHAR(255),
+    publication_year INT
+);
+
+CREATE TABLE book_author (
+    author_id BIGINT NOT NULL,
+    book_id BIGINT NOT NULL,
+    PRIMARY KEY (author_id, book_id),
+    FOREIGN KEY (author_id) REFERENCES book(id) ON DELETE CASCADE,
+    FOREIGN KEY (book_id) REFERENCES author(id) ON DELETE CASCADE
+)

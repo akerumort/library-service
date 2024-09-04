8f19ce1434a809d99c6f5d97b93519a6d4d883c4
Ivan Rodionov <144949516+akerumort@users.noreply.github.com>
2024-09-04 16:56:58 +0300

    added entities

---
+++ src/main/java/com/akerumort/LibraryService/entities/Author.java
@@ -0,0 +1,37 @@
+package com.akerumort.LibraryService.entities;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.Set;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@Table(name = "author")
+public class Author {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "first_name", nullable = false)
+    private String firstName;
+
+    @Column(name = "last_name", nullable = false)
+    private String lastName;
+
+    @Column(name = "country", nullable = false)
+    private String country;
+
+    @ManyToMany
+    @JoinTable(
+            name = "book_author",
+            joinColumns = @JoinColumn(name = "author_id"),
+            inverseJoinColumns = @JoinColumn(name = "book_id")
+    )
+    private Set<Book> books;
+}
---
+++ src/main/java/com/akerumort/LibraryService/entities/Book.java
@@ -0,0 +1,33 @@
+package com.akerumort.LibraryService.entities;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.Set;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@Table(name = "book")
+public class Book {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "title", nullable = false)
+    private String title;
+
+    @Column(name = "genre", nullable = false)
+    private String genre;
+
+    @Column(name = "publication_year")
+    private Integer publicationYear;
+
+    @ManyToMany(mappedBy = "books")
+    private Set<Author> authors;
+
+}
